c827ea5532c7ec89db71bebc4070215edc27946a
Dylou-Code <ceprik.dydy@gmail.com>
2025-02-04 23:25:02 +0100

    feat: add motion and animate hero section

---
+++ main.js
@@ -0,0 +1,7 @@
+import { animate } from "motion"
+
+// // Elements
+// const buttonIcon = document.querySelectorAll(".primary-button")
+
+// animate(buttonIcon, { rotate: 360 })
+console.log('test')
