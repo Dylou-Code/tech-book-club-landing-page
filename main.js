df187bea136c26ec184c7a05b4450cfc3fff3709
Dylou-Code <ceprik.dydy@gmail.com>
2025-02-10 08:16:21 +0100

    Migrate project to Vite

--- main.js
+++
@@ -1,7 +0,0 @@
-import { animate } from "motion"
-
-// // Elements
-// const buttonIcon = document.querySelectorAll(".primary-button")
-
-// animate(buttonIcon, { rotate: 360 })
-console.log('test')
